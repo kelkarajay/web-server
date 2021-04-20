30b47350e14bf000aa4c04d7dcdb243768f1873e
Ajay Kelkar <ajaykelkar@outlook.com>
2021-04-20 18:08:26 +0200

    test: Init Unit testing

---
+++ src/test/java/kelkar/ws/handler/ChannelHandlerTest.java
@@ -0,0 +1,59 @@
+package kelkar.ws.handler;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+public class ChannelHandlerTest {
+    Mockito mockito;
+
+    @BeforeAll
+    public void setup(){
+        mockito = new Mockito();
+    }
+
+    @Test
+    public void testHandleChannelAcceptNoPendingConnections() throws IOException {
+        SelectionKey selectionKey = mockito.mock(SelectionKey.class);
+        Selector selector = mockito.mock(Selector.class);
+        ServerSocketChannel serverSocketChannel = mockito.mock(ServerSocketChannel.class);
+        SocketChannel socketChannel = mockito.mock(SocketChannel.class);
+
+        mockito.when(selectionKey.channel()).thenReturn(serverSocketChannel);
+        mockito.when(serverSocketChannel.accept()).thenReturn(null);
+
+        ChannelHandler.handleChannelAccept(selector, selectionKey);
+    }
+
+    @Test
+    public void testHandleChannelAccept() throws IOException {
+        SelectionKey selectionKey = mockito.mock(SelectionKey.class);
+        Selector selector = mockito.mock(Selector.class);
+        ServerSocketChannel serverSocketChannel = mockito.mock(ServerSocketChannel.class);
+        SocketChannel socketChannel = mockito.mock(SocketChannel.class);
+
+        mockito.when(selectionKey.channel()).thenReturn(serverSocketChannel);
+        mockito.when(serverSocketChannel.accept()).thenReturn(socketChannel);
+
+
+        // verify(socketChannel).register(selector, SelectionKey.OP_READ);
+
+        ChannelHandler.handleChannelAccept(selector, selectionKey);
+
+        verify(socketChannel, times(1)).configureBlocking(false);
+    }
+}
